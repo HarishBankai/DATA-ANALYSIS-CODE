ebcdeee5ede0ce534927eb7844afa5ba5c6f030d
HarishBankai <harishambalgikar@gmail.com>
2026-01-18 12:43:18 +0530

    Add files via VSC

---
+++ Linear Regression.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import seaborn as sns
+import numpy as np 
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+
+np.random.seed(42)
+X = np.random.rand(200, 1)
+y = 5 + 3 * X + np.random.normal(0, 0.2, (200, 1))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+plt.figure(figsize=(10, 10))
+sns.scatterplot(x=X_test.flatten(), y=y_test.flatten(), color='blue', label = 'Training Data')
+plt.plot(X_test.flatten(), y_pred.flatten(), linewidth=2, color='red', label = 'Test Data')
+plt.xlabel('X values')
+plt.ylabel('y values')
+plt.title('Linear Regression Fit')
+plt.legend()
+plt.show()
+
+print('Intercept:', model.intercept_)
+print('Coefficient:', model.coef_)
+
+mean_squared_error_value = mean_squared_error(y_test.flatten(), y_pred.flatten())
+r2_score_value = r2_score(y_test.flatten(), y_train.flatten())
+
+print("Mean Squared Error:", mean_squared_error_value)
+print("R^2 Score:", r2_score_value)
